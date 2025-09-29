7ae21f77bbb6d085165be37c78fa267ee0f162cd
kelsierdamg <angelmartinguajardo@outlook.com>
2025-09-29 18:54:43 +0200

    Agregar archivos de proyecto.

---
+++ MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace TrivialGeografico
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Astronomy.otf", "Astronomy");
+                    fonts.AddFont("Best-Quality.otf", "BestQuality");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
